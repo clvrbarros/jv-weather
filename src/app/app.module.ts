d28a106e4098d88515ee49f004a6553a213d704b
Cleverson Barros <cleversonferreirab@gmail.com>
2020-11-10 15:13:54 -0300

    add effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,8 +20,10 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     HomeModule,
     BookmarksModule,
+    HttpClientModule,
     StoreModule.forRoot({}, {}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,16 +4,22 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { StoreModule } from '@ngrx/store';
 
-import { HomePage } from './home.page';
+import { HomePage } from './containers/home/home.page';
 import { homeReducer } from './state/home.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { HomeEffects } from './state/home.effects';
+import { ComponentsModule } from 'src/app/shared/components/components.module';
+import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
 
 
 @NgModule({
-  declarations: [HomePage],
+  declarations: [HomePage, CurrentWeatherComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     StoreModule.forFeature('home', homeReducer),
+    EffectsModule.forFeature([HomeEffects]),
+    ComponentsModule,
   ]
 })
 export class HomeModule { }
