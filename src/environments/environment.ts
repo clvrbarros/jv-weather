87fad2eb41746763a6ee4c09a4f4f8cb9e5d6936
Cleverson Barros <cleversonferreirab@gmail.com>
2020-11-10 11:10:06 -0300

    add apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'ee40853b99ca026faa7beafd64491e2e',
 };
 
 /*
